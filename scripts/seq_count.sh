ab3d48e81c070e0e9f0a1269a5664941e63075f3
Chris <calandryll@gmail.com>
2013-09-05 11:40:54 -0400

    Add some files

---
+++ scripts/seq_count.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Count the number of sequences in each fastq file
+# Grep from RNA Abundance Analysi (pg 210)
+
+FASTQ_DIR=~/transcriptome/fastq/
+
+grep -rc -P "^\@[\w:-]+?\:\w+?\s[12]\:.+" $FASTQ_DIR > sequence_count.txt
+grep -rc -P "^\+([\w:-]+?\:\w+?\s[12]\:.+|\n)" $FASTQ_DIR > quality_lines.txt
