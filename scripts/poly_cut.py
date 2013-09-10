5a2aa253fc52017e98cc778189b298ac4c9ce68a
Chris <calandryll@gmail.com>
2013-09-10 11:42:04 -0400

    Update poly_cut.py

--- scripts/poly_cut.py
+++ scripts/poly_cut.py
@@ -23,48 +23,48 @@ fastq_outdir = "/home/chris/transcriptome/fastq/poly"
 
 # Sample 1
 print "Analyzing Sample 1..."
-os.system("cutadapt -m 20 -q 20 -a %s -a %s --times=2 -o %s/Sample_1_L001_poly.fastq %s/Sample_1_L001_trimmed.fastq > %s/Sample_1_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20 -a %s -a %s --times=2 -o %s/Sample_1_L001_poly.fastq %s/Sample_1_L001_shorten.fastq > %s/Sample_1_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 1..."
-os.system("cutadapt -m 20 -q 20 -a %s -a %s --times=2 -o %s/Sample_1_L002_poly.fastq %s/Sample_1_L002_trimmed.fastq > %s/Sample_1_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20 -a %s -a %s --times=2 -o %s/Sample_1_L002_poly.fastq %s/Sample_1_L002_shorten.fastq > %s/Sample_1_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 2
 print "Analyzing Sample 2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_2_L001_poly.fastq %s/Sample_2_L001_trimmed.fastq > %s/Sample_2_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_2_L001_poly.fastq %s/Sample_2_L001_shorten.fastq > %s/Sample_2_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 2_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_2_L002_poly.fastq %s/Sample_2_L002_trimmed.fastq > %s/Sample_2_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_2_L002_poly.fastq %s/Sample_2_L002_shorten.fastq > %s/Sample_2_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 3
 print "Analyzing Sample 3..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_3_L001_poly.fastq %s/Sample_3_L001_trimmed.fastq > %s/Sample_3_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_3_L001_poly.fastq %s/Sample_3_L001_shorten.fastq > %s/Sample_3_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 3_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_3_L002_poly.fastq %s/Sample_3_L002_trimmed.fastq > %s/Sample_3_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_3_L002_poly.fastq %s/Sample_3_L002_shorten.fastq > %s/Sample_3_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 4
 print "Analyzing Sample 4..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_4_L001_poly.fastq %s/Sample_4_L001_trimmed.fastq > %s/Sample_4_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_4_L001_poly.fastq %s/Sample_4_L001_shorten.fastq > %s/Sample_4_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 4_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_4_L002_poly.fastq %s/Sample_4_L002_trimmed.fastq > %s/Sample_4_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_4_L002_poly.fastq %s/Sample_4_L002_shorten.fastq > %s/Sample_4_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 5
 print "Analyzing Sample 5..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_5_L001_poly.fastq %s/Sample_5_L001_trimmed.fastq > %s/Sample_5_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_5_L001_poly.fastq %s/Sample_5_L001_shorten.fastq > %s/Sample_5_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 5_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_5_L002_poly.fastq %s/Sample_5_L002_trimmed.fastq > %s/Sample_5_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_5_L002_poly.fastq %s/Sample_5_L002_shorten.fastq > %s/Sample_5_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 6
 print "Analyzing Sample 6..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_6_L001_poly.fastq %s/Sample_6_L001_trimmed.fastq > %s/Sample_6_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_6_L001_poly.fastq %s/Sample_6_L001_shorten.fastq > %s/Sample_6_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 6_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_6_L002_poly.fastq %s/Sample_6_L002_trimmed.fastq > %s/Sample_6_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_6_L002_poly.fastq %s/Sample_6_L002_shorten.fastq > %s/Sample_6_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 7
 print "Analyzing Sample 7..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_7_L001_poly.fastq %s/Sample_7_L001_trimmed.fastq > %s/Sample_7_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_7_L001_poly.fastq %s/Sample_7_L001_shorten.fastq > %s/Sample_7_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 7_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_7_L002_poly.fastq %s/Sample_7_L002_trimmed.fastq > %s/Sample_7_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_7_L002_poly.fastq %s/Sample_7_L002_shorten.fastq > %s/Sample_7_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 
 # Sample 8
 print "Analyzing Sample 8..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_8_L001_poly.fastq %s/Sample_8_L001_trimmed.fastq > %s/Sample_8_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_8_L001_poly.fastq %s/Sample_8_L001_shorten.fastq > %s/Sample_8_L001_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
 print "Analyzing Sample 8_2..."
-os.system("cutadapt -m 20 -q 20 -b %s --times=2 -o %s/Sample_8_L002_poly.fastq %s/Sample_8_L002_trimmed.fastq > %s/Sample_8_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
+os.system("cutadapt -m 20 -q 20  -a %s -a %s  --times=2 -o %s/Sample_8_L002_poly.fastq %s/Sample_8_L002_shorten.fastq > %s/Sample_8_L002_poly.log" % (polya, polyt, fastq_outdir, fastq_indir, fastq_outdir))
