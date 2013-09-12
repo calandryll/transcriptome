9591eb939b9b2934a31335686c78fb093bad337a
Chris <calandryll@gmail.com>
2013-09-12 12:54:56 -0400

    Update

--- scripts/ref_align.py
+++ scripts/ref_align.py
@@ -7,7 +7,7 @@ import os
 import glob
 
 # Directories for input and output
-input_dir = "/home/chris/transcriptome/fastq/poly/lane 1/"
+input_dir = "/home/chris/transcriptome/fastq/poly/"
 reference = "/home/chris/transcriptome/fastq/reference/H_akashiwo"
 output_dir = "/home/chris/transcriptome/fastq/align"
 
@@ -22,7 +22,7 @@ print "Directory: %s\n" % (input_dir)
 print "Scanning Directory..."
 
 # Pull file names from raw
-fastq_files = sorted(glob.glob1(input_dir, "*.fastq"))
+fastq_files = sorted(glob.glob(input_dir'/*/*.fastq'))
 #print fastq_files
 
 trim = len(list(fastq_files))
