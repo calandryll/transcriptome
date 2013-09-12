34aa6ea114e23920ea5cc1bb1d2ec543e684a53b
Chris <calandryll@gmail.com>
2013-09-12 11:22:05 -0400

    Update'

--- scripts/ref_align.py
+++ scripts/ref_align.py
@@ -4,11 +4,34 @@
 
 # Import OS features to run external programs
 import os
+import glob
 
 # Directories for input and output
 input_dir = "/home/chris/transcriptome/fastq/poly"
 reference = "/home/chris/transcriptome/fastq/reference/H_akashiwo"
 output_dir = "/home/chris/transcriptome/fastq/align"
 
-# Run tophat with 4 threads, should be slightly faster than the default 1 thread
-os.system("tophat -p 4 -o %s/lane\ 1/Sample_1 %s %s/lane\ 1/Sample_1_L001_poly_any.fastq" % (output_dir, reference, input_dir))
+# Setup directories to the data
+#fastq_raw = "/home/chris/transcriptome/fastq/original/"
+fastq_trimmed = "/home/chris/transcriptome/fastq/poly/"
+#fastq_routput = "/home/chris/transcriptome/fastq/reports/raw"
+fastq_toutput = "/home/chris/transcriptome/fastq/reports/poly/"
+
+#print "Raw Directory: %s\n" % (fastq_raw)
+print "Directory: %s\n" % (input_dir)
+print "Scanning Directory..."
+
+# Pull file names from raw
+fastq_tfiles = glob.glob1(input_dir, "*.fastq")
+print fastq_tfiles
+
+trim = len(list(fastq_tfiles))
+for files in range(trim):
+	print fastq_tfiles[files]
+
+	fastqc_trun = input_dir + fastq_tfiles[files]
+	
+	# Run fastqc
+	#os.system("fastqc -o %s %s" % (fastq_toutput, fastqc_trun))
+	# Run tophat with 4 threads, should be slightly faster than the default 1 thread
+	os.system("tophat -p 4 -o %s"/lane 1/Sample_1" %s %s" % (output_dir, reference, input_dir))
