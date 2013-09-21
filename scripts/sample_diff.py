f549cbf8ff427d71c95e07816475bb24fe023781
Chris <calandryll@gmail.com>
2013-09-21 14:32:44 -0400

    Update

---
+++ scripts/sample_diff.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python -tt
+# Uses cuffdiff to measure differences between controls and treatments
+# Samples 1 - 4 - Controls (No Vibrio, 1uM Iron)
+# Samples 5 - 8 - Treatments (Vibrio, 1uM Iron)
+# Website:  http://cufflinks.cbcb.umd.edu/manual.html#cuffdiff
+
+# Import OS features to run external programs
+import os
+import glob
