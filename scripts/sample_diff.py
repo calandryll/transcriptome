ba3cee71d096547044302b73634c1f46e87ea9c2
Chris <calandryll@gmail.com>
2013-09-21 18:06:17 -0400

    Update

--- scripts/sample_diff.py
+++ scripts/sample_diff.py
@@ -7,3 +7,19 @@
 # Import OS features to run external programs
 import os
 import glob
+
+# Directories
+merge_lane1 = "/home/chris/transcriptome/fastq/merged/lane1/merged.gtf"
+merge_lane2 = "/home/chris/transcriptome/fastq/merged/lane2/merged.gtf"
+output_dir = "/home/chris/transcriptome/fastq/diff/"
+input_dir = "/home/chris/transcriptome/fastq/align/"
+
+
+#cuffdiff merged_asm/merged.gtf liver1.bam,liver2.bam brain1.bam,brain2.bam
+# Pull files from directory
+control_lane1 = sorted(glob.glob(input_dir + "/*/lane1/controls/*"))
+
+print control_lane1
+
+
+#os.system("cuffdiff -p 4 -o %s %s %s %s" % (output_dir, merge_lane1, controls, treat))
